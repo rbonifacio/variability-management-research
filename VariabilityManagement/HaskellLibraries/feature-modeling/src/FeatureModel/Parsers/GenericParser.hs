c63b07369225aaacd6db7aea59a5c332475c75fd
rbonifacio123 <rbonifacio123@6ffe8d01-9945-0410-855c-3f963aebcd54>
2009-07-13 23:36:58 +0000

    Parser retornando Success ou Fail.

--- VariabilityManagement/HaskellLibraries/feature-modeling/src/FeatureModel/Parsers/GenericParser.hs
+++ VariabilityManagement/HaskellLibraries/feature-modeling/src/FeatureModel/Parsers/GenericParser.hs
@@ -6,6 +6,7 @@ module FeatureModel.Parsers.GenericParser (
 where 
 
 import FeatureModel.Types
+import qualified BasicTypes as Core
 
 -- modules related to the FMPlugin parser
 import FeatureModel.Parsers.FMPlugin.XmlFeatureParser 
@@ -38,6 +39,10 @@ import Text.XML.HXT.Arrow
 
 data FmFormat = FMPlugin | FMIde | FMGrammar | SXFM
 
+-- |
+-- The top most function for parsing feature models 
+-- in different formats. 
+-- 
 parseFeatureModel fileName format = do
  x <- readFile (fileName) 
  case (format) of 
@@ -56,31 +61,40 @@ parseFeatureModel fileName format = do
   SXFM  -> do
     r <- parseFromFile ParsecSXFM.parseFeatureModel fileName ; 
     case (r) of
-      Left err  -> error (show err)
+      Left err  -> return $ Core.Fail (show err)
       Right f  -> do let fm = f
-                     return fm
+                     return $ Core.Success fm
 
+-- | 
+-- Parse a feature configuration. This parser 
+-- is based on the exported instance models from 
+-- FMPlugin
+--
 parseInstanceConfiguration fileName = 
  do 
-   [x] <- runX ( xunpickleDocument xpFeatureConfiguration [ (a_validate,v_0)
+   i <- runX ( xunpickleDocument xpFeatureConfiguration [ (a_validate,v_0)
  					                  , (a_trace, v_1)
  					                  , (a_remove_whitespace,v_1)
  					                  , (a_preserve_comment, v_0)
  					                  ] fileName)
-   let ic = xml2FeatureConfiguration x
-   return ic
+   case i of 
+    [x] -> do return $ Core.Success (xml2FeatureConfiguration x)
+    otherwise -> return $ Core.Fail "Error parsing feature model. Try to check it before parsing."
+    
 
-translateFMIdeToFm (Ok g)  = grammarToFeatureModel g
-translateFMIdeToFm (Bad s) = error s
+translateFMIdeToFm (Ok g)  = Core.Success (grammarToFeatureModel g)
+translateFMIdeToFm (Bad s) = Core.Fail s
 
-translateFMGrammarToFm (EFMG.Ok g) = GFMG.grammarToFeatureModel g  
-translateFMGrammarToFm (EFMG.Bad s) = error s
+translateFMGrammarToFm (EFMG.Ok g) = Core.Success (GFMG.grammarToFeatureModel g)  
+translateFMGrammarToFm (EFMG.Bad s) = Core.Fail s
 
 translateFMPToFm s = 
  do
-   [x] <- runX ( xunpickleDocument xpFeature [ (a_validate,v_0)
-                                             , (a_trace, v_1)
-                                             , (a_remove_whitespace,v_1)
-                                             , (a_preserve_comment, v_0)
-                                             ] s);
-   return FeatureModel { fmTree = (xmlFeature2FeatureTree x), fmConstraints = [] }
+   u <- runX ( xunpickleDocument xpFeature [ (a_validate,v_0)
+                                           , (a_trace, v_1)
+                                           , (a_remove_whitespace,v_1)
+                                           , (a_preserve_comment, v_0)
+                                           ] s);
+   case u of 
+     [x] -> return $ Core.Success (FeatureModel { fmTree = (xmlFeature2FeatureTree x), fmConstraints = [] })
+     otherwise -> return $ Core.Fail "Error parsing feature model. Try to check it before parsing."
